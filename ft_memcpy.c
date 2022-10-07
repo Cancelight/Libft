0bde6a20a64fe3ecf192c4c525cde9d22cf2e9dc
Berna Kiziler <bkiziler@k1m35s08.42istanbul.com.tr>
2022-10-07 21:17:12 +0300

    aa

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,17 +6,26 @@
 /*   By: bkiziler <bkiziler@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/07 10:56:04 by bkiziler          #+#    #+#             */
-/*   Updated: 2022/10/07 11:53:57 by bkiziler         ###   ########.fr       */
+/*   Updated: 2022/10/07 18:28:03 by bkiziler         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-
+#include <stdio.h>
 
 void	*ft_memcpy(void *dst, const void *src, size_t n)
 {
-	while(*src)
+	int i;
+	i = 0;
+	unsigned char *a;
+	unsigned char *b;
+	a= (unsigned char *)dst;
+	b=(unsigned char *)src;
+
+	while(i < n)
 	{
-		
+			a[i] = b[i];
+			i++;
 	}
+	return (a);
 }

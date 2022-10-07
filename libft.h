85ea6d626b4dc2edc83a1d34c6679d1bf46687c7
Berna Kiziler <bkiziler@k1m35s08.42istanbul.com.tr>
2022-10-07 21:19:37 +0300

    lib

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: bkiziler <bkiziler@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/07 11:26:30 by bkiziler          #+#    #+#             */
-/*   Updated: 2022/10/07 11:26:45 by bkiziler         ###   ########.fr       */
+/*   Updated: 2022/10/07 21:18:53 by bkiziler         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,5 +20,10 @@ int	ft_isdigit(int c);
 int	ft_isalnum(int c);
 int	ft_isascii(int c);
 int	ft_isprint(int c);
+size_t	ft_strlen(const char *s);
+void	*ft_memset(void *b, int c, size_t len);
+void	*ft_memmove(void *dst, const void *src, size_t len);
+void	*ft_memcpy(void *dst, const void *src, size_t n);
+void	bzero(void *s, size_t n);
 
 #endif

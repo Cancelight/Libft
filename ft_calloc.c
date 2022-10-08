f8903efb7575af2830b09fdab8cb902592a742b1
Berna Kiziler <bkiziler@k1m35s08.42istanbul.com.tr>
2022-10-08 18:49:00 +0300

    aa

--- ft_calloc.c
+++ ft_calloc.c
@@ -1,42 +1,15 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*   ft_calloc.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: bkiziler <bkiziler@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/10/08 14:37:13 by bkiziler          #+#    #+#             */
-/*   Updated: 2022/10/08 18:07:13 by bkiziler         ###   ########.fr       */
+/*   Created: 2022/10/08 18:08:35 by bkiziler          #+#    #+#             */
+/*   Updated: 2022/10/08 18:15:38 by bkiziler         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-int ft_atoi (const char *str)
-{
-	int sign;
-	int i;
-	int number;
 
-	number = 0;
-	sign = 1;
-	i = 0;
-	while(str[i] == 32 || str[i] == '\t')
-	{
-		i++;
-	}
-	if (str[i] == 43 || str[i] == 45)
-	{
-		if (str[i] == 45)
-			sign *= -1;
-		i++;
-	}
-	if (str[i] <= '0' && str[i] >= '9')
-		return (0);
-	while(str[i] >= '0' && str[i] <= '9')
-	{
-		number = (str[i] - 48) + (number * 10);
-		i++;
-	}
-	return (number * sign);
 
-}

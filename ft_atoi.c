f8cd3efcf487020c61d90d992ad5f5bcad971012
Berna Kiziler <bkiziler@k1m35s08.42istanbul.com.tr>
2022-10-08 15:43:30 +0300

    avokado adam B)

---
+++ ft_atoi.c
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bkiziler <bkiziler@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/08 14:37:13 by bkiziler          #+#    #+#             */
+/*   Updated: 2022/10/08 15:43:08 by bkiziler         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+int ft_atoi (const char *str)
+{
+	int sign;
+	int i;
+	int number;
+
+	number = 0;
+	sign = 1;
+	i = 0;
+	while(str[i] == 32 || str[i] == '\t')
+	{
+		i++;
+	}
+	if (str[i] == 43 || str[i] == 45)
+	{
+		if (str[i] == 45)
+			sign *= -1;
+		i++;
+	}
+	if (str[i] <= '0' && str[i] >= '9')
+		return (0);
+	while(str[i] >= '0' && str[i] <= '9')
+	{
+		number = (str[i] - 48) + (number * 10);
+		i++;
+	}
+	return (number * sign);
+
+}
---
+++ ft_strnstr.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strnstr.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bkiziler <bkiziler@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/08 09:58:24 by bkiziler          #+#    #+#             */
+/*   Updated: 2022/10/08 12:53:57 by bkiziler         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char *strnstr(const char *haystack, const char *needle, size_t len)
+{
+	size_t i;
+	size_t b;
+
+	i = 0;
+	b = 0;
+
+	if (ft_strlen(needle) == 0)
+		return (haystack);
+	while(haystack[i])
+	{
+		while (haystack [i + b] == needle[b] && (i + b) < len)
+		{
+			if (needle[b + 1] == '\0')
+				return (haystack);
+			b++;
+		}
+	i++;
+	}
+	return (0);
+}

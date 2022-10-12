87679f4f4fb77f8ec15059b67ba34bfdd4915c80
Cancelight <bernakiziler@gmail.com>
2022-10-12 14:32:17 +0300

    aaa

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -5,9 +5,44 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: bkiziler <bkiziler@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/10/11 14:12:16 by bkiziler          #+#    #+#             */
-/*   Updated: 2022/10/11 14:12:40 by bkiziler         ###   ########.fr       */
+/*   Created: 2022/10/12 14:19:57 by bkiziler          #+#    #+#             */
+/*   Updated: 2022/10/12 14:32:07 by bkiziler         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+int check(char c, char const *ptr)
+{
+    int i;
+    i = 0;
+    while (ptr[i])
+    {
+        if (c == ptr[i])
+            return(0);
+        i++;
+    }
+    return (1); 
+}
+char *ft_strtrim(char const *s1, char const *set)
+{
+    char *ptr;
+    int i;
+    int n;
+
+    i = 0;
+    n = ft_strlen(s1) - 1;
+    while(s1[i])
+    {
+        if (check (s1[i], set))
+            break;
+        i++;
+    }
+    while(n >= 0)
+    {
+        if (check (s1[n], set))
+            break;
+        n--;
+    }
+    ptr = ft_substr(s1, i, n - i + 1);
+    return(ptr);   
+}

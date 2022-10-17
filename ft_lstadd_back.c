1dd55a64e4da09180ee0e3b012c28d744798827a
Berna Kiziler <bkiziler@k1m35s02.42istanbul.com.tr>
2022-10-17 14:47:53 +0300

    welp

--- ft_lstadd_back.c
+++ ft_lstadd_back.c
@@ -6,7 +6,7 @@
 /*   By: bkiziler <bkiziler@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/16 19:34:57 by bkiziler          #+#    #+#             */
-/*   Updated: 2022/10/17 14:08:39 by bkiziler         ###   ########.fr       */
+/*   Updated: 2022/10/17 14:33:59 by bkiziler         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,6 @@ void ft_lstadd_back(t_list **lst, t_list *new)
 		return(0);
 	if (*lst == NULL)
 		*lst = new;
-	else
 	{
 		t_list * a;
 		a = ft_lstlast(*lst);

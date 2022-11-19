f0befe6bd0cb119eaf0c0f67c8f09cc00e70284f
Berna Kiziler <bkiziler@k1m15s01.42istanbul.com.tr>
2022-11-19 13:17:45 +0300

    a

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,13 +6,11 @@
 /*   By: bkiziler <bkiziler@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/08 14:37:13 by bkiziler          #+#    #+#             */
-/*   Updated: 2022/11/19 13:16:47 by bkiziler         ###   ########.fr       */
+/*   Updated: 2022/11/19 13:17:33 by bkiziler         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-
-int a;
 
 int	ft_atoi(const char *s)
 {
